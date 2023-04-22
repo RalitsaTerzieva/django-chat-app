14a0ac259088bae8f10a10b3bd62a0d4c363a77d
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-22 18:24:15 +0500

    Add index page functionality

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import ChatRoom
+
 
 def index(request):
-    return render(request,'chatapp/index.html')
+    chat_rooms = ChatRoom.objects.all()
+    return render(request,'chatapp/index.html',{'chat_rooms':chat_rooms})
