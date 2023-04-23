dbd2f966c016ab64a7e02209f4fb5304014fd8b0
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-23 20:39:31 +0300

    Add logic for sending a message from the client

--- chatapp/consumer.py
+++ chatapp/consumer.py
@@ -16,7 +16,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         
         await self.accept()
         
-    async def disconnect(self):
+    async def disconnect(self, room_name):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
